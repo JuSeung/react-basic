2e5789d68c6b58128d396ef5f7d2a31d6a305c1d
jsshin <jsshin6246@gmail.com>
2024-10-08 21:58:58 +0900

    add: useState 연습

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Profile from "./components/Profile";
+import Avartar from "./components/Avartar";
 
 function AppProfile() {
+    const handleClick = (event) => {
+        console.log(event);
+        alert('버튼이 클릭함')
+    };
     return (
         <>
+            <button onClick={handleClick}>버튼</button>
+            <Avartar image='https://plus.unsplash.com/premium_photo-1724121437148-7d87478f505e?q=80&w=4400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' isNew={true}/>
             <Profile image='https://plus.unsplash.com/premium_photo-1724121437148-7d87478f505e?q=80&w=4400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
                      name='James Kim'
                      title='프론트엔드 개발자'
