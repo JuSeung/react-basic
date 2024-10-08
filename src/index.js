a112353e931b640eca4ec65a0d504b5c988d5221
jsshin <jsshin6246@gmail.com>
2024-10-08 22:32:25 +0900

    add: data 위치 public 하위로 이동

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppProfile from './AppProfile';
-import AppProducts from "./AppProducts";
 import AppCounter from "./AppCounter";
+import AppProducts from "./AppProducts";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <AppCounter/>
+      <AppProducts/>
   </React.StrictMode>
 );
 
